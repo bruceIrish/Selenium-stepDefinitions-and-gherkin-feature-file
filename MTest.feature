fc59dfc8659347c1be9d0424017b74b118cf324e
birish <bruce.irish@philips.com>
2016-02-02 03:30:55 -0500

    Added Selenium files
    Selenium Step Definitions, written in C#  and Gherkin feature file.

---
+++ MTest.feature
@@ -0,0 +1,34 @@
+﻿Feature: MobyMax Registration
+Feature details
+	
+@RegistrationTest_EndToEnd
+Scenario Outline: End to End registration
+	Description: test registration
+
+	Given I have navigated to URL <URL> in browser <browser>
+	When I have entered firstname <firstname> into <firstname field> and lastname <lastname> into <lastname field>
+	And I have entered educator <educator> into <educator checkbox> of attribute "data-checked" 
+	And I have entered zipcode <zipcode> into <zipcode field> and school <schoolId> into <school field>
+	And I have entered email address <email address> into <email field> and password <password> into <password field>
+	And I press the Register button identified by <register xpath>
+	Then the result welcome page identified by dismissing the modal <welcome modal> contains title <welcome xpath> matching <firstname> <lastname> and URL is <WelcomeURL>
+	
+Examples: 
+| URL                     | browser | firstname | firstname field                               | lastname | lastname field                                | educator | educator checkbox                                  | zipcode | zipcode field                                 | schoolId | school field                                     | email address    | email field                                    | password | password field                                     | register xpath             | welcome modal                      | welcome xpath      | WelcomeURL                           |
+| http://www.mobymax.com/ | IE      | John      | //*[@id="register-element-form"]/div[1]/input | Doe1     | //*[@id="register-element-form"]/div[2]/input | teacher  | //*[@id="register-element-form"]/div[3]/div[1]/img | 15122   | //*[@id="register-element-form"]/div[8]/input | 13560    | //*[@id="register-element-form"]/div[9]/input[1] | jdoe586@gmail.com | //*[@id="register-element-form"]/div[13]/input | abc123   | //*[@id="register-element-form"]/div[14]/div/input | //*[@id="register-button"] | //*[@id="video-dialog"]/div/div[2] | //*[@id="welcome"] | http://www.mobymax.com/MM/MT/Welcome |
+| http://www.mobymax.com/ | Chrome  | John      | //*[@id="register-element-form"]/div[1]/input | Doe 2    | //*[@id="register-element-form"]/div[2]/input | teacher  | //*[@id="register-element-form"]/div[3]/div[1]/img | 15122   | //*[@id="register-element-form"]/div[8]/input | 13560    | //*[@id="register-element-form"]/div[9]/input[1] | jdoe70@gmail.com  | //*[@id="register-element-form"]/div[13]/input | jd12342  | //*[@id="register-element-form"]/div[14]/div/input | //*[@id="register-button"] | //*[@id="video-dialog"]/div/div[1] | //*[@id="welcome"] | http://www.mobymax.com/MM/MT/Welcome |
+#| http://www.mobymax.com/ | Safari  | John      | //*[@id="register-element-form"]/div[1]/input | Doe 3    | //*[@id="register-element-form"]/div[2]/input | teacher  | //*[@id="register-element-form"]/div[3]/div[1]/img | 15122   | //*[@id="register-element-form"]/div[8]/input | 13560    | //*[@id="register-element-form"]/div[9]/input[1] | jdoe67@gmail.com  | //*[@id="register-element-form"]/div[13]/input | jd12343  | //*[@id="register-element-form"]/div[14]/div/input | //*[@id="register-button"] | //*[@id="video-dialog"]/div/div[1] | //*[@id="welcome"] | http://www.mobymax.com/MM/MT/Welcome |
+#| http://www.mobymax.com/ | Firefox | John      | //*[@id="register-element-form"]/div[1]/input | Doe 4    | //*[@id="register-element-form"]/div[2]/input | teacher  | //*[@id="register-element-form"]/div[3]/div[1]/img | 15122   | //*[@id="register-element-form"]/div[8]/input | 13560    | //*[@id="register-element-form"]/div[9]/input[1] | jdoe68@gmail.com  | //*[@id="register-element-form"]/div[13]/input | jd12344  | //*[@id="register-element-form"]/div[14]/div/input | //*[@id="register-button"] | //*[@id="video-dialog"]/div/div[1] | //*[@id="welcome"] | http://www.mobymax.com/MM/MT/Welcome |
+
+@mytag
+Scenario Outline: Test If School dropdown list populates with appropriate schools upon entering correct zip code
+	Given I have navigated to the URL <URL> in browser <browser>
+	When I enter the zip code <zipcode> into <zipcode field> and click the school dropdown <school field> containing <List of Schools per zip>
+	Then School dropdown list should include the list of schools <List of Schools per zip>
+
+Examples: 
+	| URL                     | browsers | zipcode | zipcode field                                 | school field                                     | List of Schools per zip                                                                                                                                                                                                                                               |
+	| http://www.mobymax.com/ | IE       | 15122   | //*[@id="register-element-form"]/div[8]/input | //*[@id="register-element-form"]/div[9]/input[1] | Calvary Baptist Preschool & Kind, Clara Barton El Sch, Early Childhood Education Center,Homeville El Sch,New Emerson El Sch,New England El Sch,St Agnes School,Walnut Grove Christian School,West Mifflin Area Hs,Wilson Christian Academy,School not listed?         |
+	| http://www.mobymax.com/ | Chrome   | 15122   | //*[@id="register-element-form"]/div[8]/input | //*[@id="register-element-form"]/div[9]/input[1] | Calvary Baptist Preschool & Kind, Clara Barton El Sch, Early Childhood Education Center, Homeville El Sch, New Emerson El Sch, New England El Sch, St Agnes School, Walnut Grove Christian School, West Mifflin Area Hs, Wilson Christian Academy, School not listed? |
+	#| http://www.mobymax.com/ | Safari   | 15122   | //*[@id="register-element-form"]/div[8]/input | //*[@id="register-element-form"]/div[9]/input[1] | Calvary Baptist Preschool & Kind, Clara Barton El Sch, Early Childhood Education Center, Homeville El Sch, New Emerson El Sch, New England El Sch, St Agnes School, Walnut Grove Christian School, West Mifflin Area Hs, Wilson Christian Academy, School not listed? |
+	#| http://www.mobymax.com/ | Firefox  | 15122   | //*[@id="register-element-form"]/div[8]/input | //*[@id="register-element-form"]/div[9]/input[1] | Calvary Baptist Preschool & Kind, Clara Barton El Sch, Early Childhood Education Center, Homeville El Sch, New Emerson El Sch, New England El Sch, St Agnes School, Walnut Grove Christian School, West Mifflin Area Hs, Wilson Christian Academy, School not listed? |
---
+++ MobyMaxRegistrationTestSteps.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Safari;
+using TechTalk.SpecFlow;
+
+namespace MobyRegistrationDemo
+{
+    [Binding]
+    public class MobyMaxRegistrationSteps
+    {
+        IWebDriver driver;
+        private string OutputBrowserName;
+        
+      
+        [Given(@"I have navigated to URL (.*) in browser (.*)")]
+        public void GivenIHaveNavigatedToTheMobyMaxRegistrationPageInBrowser(string url, string browser)
+        {
+            if (browser == "Chrome")
+            {
+                driver = new ChromeDriver(@"C:\Users\310177086\Documents\Visual Studio 2013\Projects\MobyRegistrationDemo");
+            }
+            else if (browser == "Safari")
+            {
+                driver = new SafariDriver();
+            }
+            else if (browser == "Firefox")
+            {
+                driver = new FirefoxDriver();
+            }
+            else if (browser == "IE")
+            {
+                driver = new InternetExplorerDriver(@"C:\Users\310177086\Documents\Visual Studio 2013\Projects\MobyRegistrationDemo");
+
+            }
+
+            OutputBrowserName = browser;
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl(url);
+
+        }
+
+
+        [When(@"I have entered firstname (.*) into (.*) and lastname (.*) into (.*)")]
+        public void WhenIHaveEnteredFirstAndLastName(string firstNameText, string firstNameField, string lastNameText, string lastNameField)
+        {
+            EnterTextIntoTextBox(firstNameText, firstNameField);
+            EnterTextIntoTextBox(lastNameText, lastNameField);
+        }
+
+        [When(@"I have entered educator (.*) into (.*) of attribute (.*)")]
+        public void WhenIHaveEnteredEducator(string educatorType, string educatorCheckBoxLocation, string stateAttribute)
+        {
+          
+            if (!driver.FindElement(By.XPath(educatorCheckBoxLocation)).Displayed)
+            {
+                IWebElement eduCheckImg = driver.FindElement(By.XPath(educatorCheckBoxLocation));
+                eduCheckImg.Click();
+            }
+            
+        }
+
+        [When(@"I have entered zipcode (.*) into (.*) and school (.*) into (.*)")]
+        public void WhenIHaveEnteredZipCodeAndSchool(string zipCode, string zipCodeField, string schoolId, string schoolField)
+        {
+            EnterTextIntoTextBox(zipCode, zipCodeField);
+
+            //click school dropdown
+            driver.FindElement(By.XPath(schoolField)).Click();
+
+            Thread.Sleep(1000);
+            //Search
+            driver.FindElement(By.Id(schoolId)).Click();
+
+        }
+
+        [When(@"I have entered zipcode (.*) into (.*) and click the school (.*) containing (.*)")]
+        public void WhenIHaveEnteredZipCodeAndCheckedSchool(string zipCode, string zipCodeField, string schoolField, string listOfSchools)
+        {
+            EnterTextIntoTextBox(zipCode, zipCodeField);
+
+            //click school dropdown
+            driver.FindElement(By.XPath(schoolField)).Click();
+
+            Thread.Sleep(1000);
+            
+            //Search In progress
+
+            SearchSchoolList(schoolField, listOfSchools);
+
+        }
+
+        [When(@"I have entered email address (.*) into (.*) and password (.*) into (.*)")]
+        public void WhenIHaveEnteredEmailAdress(string email, string emailField, string password, string passwordField)
+        {
+            EnterTextIntoTextBox(email, emailField);
+            EnterTextIntoTextBox(password, passwordField);
+        }
+
+        [When(@"I press the Register button identified by (.*)")]
+        public void WhenIPressAdd(string registerButtonLocation)
+        {
+            driver.FindElement(By.XPath(registerButtonLocation)).Click();
+        }
+
+        [Then(@"the result welcome page identified by dismissing the modal (.*) contains title (.*) matching (.*)(.*) and URL is (.*)")]
+        public void TheResultWelcomePageIdentifiedBy(string xButtonOnModal, string welcomeBanner, string firstName, string lastName, string expectedURL)
+        {
+
+            Thread.Sleep(2000);
+            
+            //wait for page to load after clicking register
+            int counter = 0;
+            bool continueLoop = true;
+
+            while (continueLoop && counter < 10)
+            {
+                try
+                {
+                    IWebElement xModal = driver.FindElement(By.XPath(xButtonOnModal));
+
+                    if (xModal.Displayed)
+                    {
+                        xModal.Click();
+                        continueLoop = false;
+                    }
+
+                    
+                }
+                catch (OpenQA.Selenium.NoSuchElementException)
+                {
+                    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2));
+                    counter++;
+                    if (counter == 9)
+                    {
+                        TakeScreenshot("Fail");
+                        Assert.Fail();
+                    }
+                }
+            }
+
+           
+            string actualURL = driver.Url;
+            string welcomeStringActual = driver.FindElement(By.XPath(welcomeBanner)).Text;
+            string welcomeStringExpected = "Welcome: " + firstName + " " + lastName;
+            welcomeStringExpected = welcomeStringExpected.Trim();
+            
+           
+            if (actualURL.Contains(expectedURL) && welcomeStringActual.Equals(welcomeStringExpected))
+            {
+                TakeScreenshot("Passed");
+                driver.Quit();
+                Assert.Pass();
+                
+            }
+            else
+            {
+               Debug.Print("ActualURL: " + actualURL + " Actual Welcome mssg: " + welcomeStringActual);
+               Debug.Print("ExpectedURL: " + expectedURL + " Expec Welcome mssg: " + welcomeStringExpected);
+               TakeScreenshot("Failed");
+                driver.Quit();
+                Assert.Fail();
+            }
+        }
+
+        
+        //Helper methods
+
+        //Find Textbox by xpath, clear it, write text into it.
+        public void EnterTextIntoTextBox(string fieldText, string xpath)
+        {
+            IWebElement textField = driver.FindElement(By.XPath(xpath));
+            textField.Clear();
+            textField.SendKeys(fieldText);
+            
+        }
+
+        public void TakeScreenshot(string PassOrFail)
+        {
+            try
+            {
+                
+                Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+                string scenarioTagData = ScenarioContext.Current.ScenarioInfo.Title;
+                ss.SaveAsFile(ConfigurationSettings.AppSettings["ScreenShotFilePath"]+"_"+scenarioTagData+PassOrFail+"_in"+"_" +OutputBrowserName+".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public void SearchSchoolList(string actualControl, string expectedList)
+        {
+            //todo
+        }
+    }
+}
